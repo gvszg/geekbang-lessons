ecf97f39e16a9ded6dc0b3a85ef3672a3447070f
jamesyu <zetaentranceserious@gmail.com>
2021-03-01 20:44:44 +0800

    feat: Implement register user service

--- projects/stage-0/user-platform/user-web/src/main/java/org/geektimes/projects/user/web/controller/RegisterFormController.java
+++ projects/stage-0/user-platform/user-web/src/main/java/org/geektimes/projects/user/web/controller/RegisterFormController.java
@@ -1,5 +1,8 @@
 package org.geektimes.projects.user.web.controller;
 
+import org.geektimes.projects.user.domain.User;
+import org.geektimes.projects.user.service.UserService;
+import org.geektimes.projects.user.serviceImpl.UserServiceImpl;
 import org.geektimes.web.mvc.controller.PageController;
 
 import javax.servlet.http.HttpServletRequest;
@@ -7,6 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import java.util.Map;
 
 @Path("/register")
 public class RegisterFormController implements PageController {
@@ -15,7 +19,15 @@ public class RegisterFormController implements PageController {
     public String execute(HttpServletRequest request, HttpServletResponse response) throws Throwable {
         System.out.println(request.getMethod());
         if (request.getMethod() == "POST") {
-           return "register-result.jsp" ;
+            Map<String, String[]> paramsMap = request.getParameterMap();
+            User user = new User();
+            user.setName(paramsMap.get("name")[0]);
+            user.setEmail(paramsMap.get("email")[0]);
+            user.setPhoneNumber(paramsMap.get("phoneNumber")[0]);
+            user.setPassword(paramsMap.get("password")[0]);
+            UserService userService = new UserServiceImpl();
+            userService.register(user);
+            return "register-result.jsp" ;
         }
         return "register-form.jsp";
     }
