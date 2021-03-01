7f4c71bc029937e2efb922387bf9427e96594836
jamesyu <zetaentranceserious@gmail.com>
2021-03-01 15:57:27 +0800

    feat: Add POST reister url path

--- projects/stage-0/user-platform/user-web/src/main/java/org/geektimes/projects/user/web/controller/RegisterFormController.java
+++ projects/stage-0/user-platform/user-web/src/main/java/org/geektimes/projects/user/web/controller/RegisterFormController.java
@@ -5,13 +5,18 @@ import org.geektimes.web.mvc.controller.PageController;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 
-@Path("/register_form")
+@Path("/register")
 public class RegisterFormController implements PageController {
     @GET
-//    @Path("/user")
+    @POST
     public String execute(HttpServletRequest request, HttpServletResponse response) throws Throwable {
+        System.out.println(request.getMethod());
+        if (request.getMethod() == "POST") {
+           return "register-result.jsp" ;
+        }
         return "register-form.jsp";
     }
 }
